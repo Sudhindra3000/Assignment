8788e30567556cfbe1275b7a7cc63724f5dd9a52
Sudhindra3000 <55095527+Sudhindra3000@users.noreply.github.com>
2025-01-24 13:54:34 +0530

    [fix]: set id as primary key do avoid duplicates

--- app/src/main/java/com/myjar/jarassignment/data/model/db/DbComputerItem.kt
+++ app/src/main/java/com/myjar/jarassignment/data/model/db/DbComputerItem.kt
@@ -1,8 +1,10 @@
 package com.myjar.jarassignment.data.model.db
 
 import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
 
 class DbComputerItem: RealmObject {
+    @PrimaryKey
     var id: String = ""
     var name: String = ""
     var data: DbItemData? = null

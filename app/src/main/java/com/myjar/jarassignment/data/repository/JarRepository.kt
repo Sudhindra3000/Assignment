947d094c4920f455575504a859c744925dc1d515
Sudhindra3000 <55095527+Sudhindra3000@users.noreply.github.com>
2025-01-24 13:20:20 +0530

    [feat]: get list from db in case of exception in fetchResults

--- app/src/main/java/com/myjar/jarassignment/data/repository/JarRepository.kt
+++ app/src/main/java/com/myjar/jarassignment/data/repository/JarRepository.kt
@@ -3,10 +3,13 @@ package com.myjar.jarassignment.data.repository
 import android.util.Log
 import com.myjar.jarassignment.data.api.ApiService
 import com.myjar.jarassignment.data.model.ComputerItem
+import com.myjar.jarassignment.data.model.ItemData
 import com.myjar.jarassignment.data.model.db.DbComputerItem
 import com.myjar.jarassignment.data.model.db.DbItemData
 import io.realm.kotlin.Realm
+import io.realm.kotlin.ext.query
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 
 interface JarRepository {
@@ -20,22 +23,33 @@ class JarRepositoryImpl(
     private val db: Realm,
 ) : JarRepository {
     override suspend fun fetchResults(): Flow<List<ComputerItem>> = flow {
-        val apiResp = apiService.fetchResults()
-        emit(apiResp)
-        db.write {
-            apiResp.forEach { apiItem ->
-                val x = copyToRealm(
-                    DbComputerItem().apply {
-                        id = apiItem.id
-                        name = apiItem.name
-                        if (apiItem.data != null)
-                            data = DbItemData().apply {
-                                color = apiItem.data.color ?: ""
-                                capacity = apiItem.data.capacity ?: ""
-                            }
-                    }
+        try {
+            val apiResp = apiService.fetchResults()
+            emit(apiResp)
+            db.write {
+                apiResp.forEach { apiItem ->
+                    copyToRealm(
+                        DbComputerItem().apply {
+                            id = apiItem.id
+                            name = apiItem.name
+                            if (apiItem.data != null)
+                                data = DbItemData().apply {
+                                    color = apiItem.data.color ?: ""
+                                    capacity = apiItem.data.capacity ?: ""
+                                }
+                        }
+                    )
+                }
+            }
+        } catch (e: Exception) {
+            Log.e(TAG, "fetchResults: ", e)
+            val dbList = db.query<DbComputerItem>().asFlow().first().list.toList().map {  dbItem ->
+                ComputerItem(
+                    id = dbItem.id,
+                    name = dbItem.name,
                 )
             }
+            emit(dbList)
         }
     }
 }
